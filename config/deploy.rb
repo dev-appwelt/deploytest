b98635c0511a0658c9dd771b7a8b0708827db1e5
vivek-deployment-server <vgangotri@theappwelt.com>
2020-05-28 07:42:46 +0000

    ..

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,11 +60,14 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-# set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :deploy_to,       "/var/www/deployment/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+# set :deploy_to,       "/var/www/deployment/#{fetch(:application)}"
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind,       "unix://#{release_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{release_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{release_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/devappwelt.pub) }
